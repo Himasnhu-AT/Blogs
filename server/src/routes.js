d5d971b1a4a6291fb197794aac79fd342d8ea9e6
Rajvansh Malhotra <rajvanshmalhotra@Rajvanshs-MacBook-Pro.local>
2024-06-24 14:06:52 +0545

    added the route for getting blog by id

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,23 @@
 // import authRoutes from "./router/auth.js";
-
+const express = require('express');
+const router = express.Router();
 function routes(app) {
     app.get("/", (req, res) => {
         res.send("Hello from server");
     });
+    router.get('/:id', async (req, res) => {
+        try {
+            const blog = await Blog.findById(req.params.id);
+            if (blog == null) {
+                return res.status(404).json({ message: 'Cannot find blog' });
+            }
+            res.json(blog);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    });
 
-    
+
 
     // app.use('/auth', authRoutes);
 }
